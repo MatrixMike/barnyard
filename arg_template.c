50ac57450ff1d5cc2e5f693f85189bd740bba18c
Mike H <matroxmike@icloud.com>
2018-06-24 15:42:17 +1000

    some use possible now

--- arg_template.c
+++ arg_template.c
@@ -14,44 +14,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define INFO  "No information available"    /* Change this */
-#define USAGE "No Usage Information available"
-#define MAX_ARG_LEN 80             /* Max length of combined options */
+#define INFO  "No information available"	/* Change this */
+#define USAGE "No Usage Information available\n"
+#define MAX_ARG_LEN 80		/* Max length of combined options */
 
-int find_opt(char *option_string);
+int find_opt (char *option_string);
 #include <stdarg.h>
-void myerror(char *fmt, ...);
+void myerror (char *fmt, ...);
 char ProgramName[256];
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
-	int c=-1;                /* numeric option code */
-	char *info = INFO;
-	char opsep = '-';      /* Standard for Unix */
-	char argstr[MAX_ARG_LEN];
+  int c = -1;			/* numeric option code */
+  char *info = INFO;
+  char opsep = '-';		/* Standard for Unix */
+  char argstr[MAX_ARG_LEN];
 
 /* Program name is available in ProgramName */
-	strcpy(ProgramName,argv[0]);
+  strcpy (ProgramName, argv[0]);
 /* Process Command Line Options */
 
-if(--argc > 0){  /* Anything on command line ? */
-                ++argv;
-                while((argc > 0)&&((*argv[0]) == opsep)){ /* loop while 
-							there are flags */
-                        argstr[0]=opsep;
-                        argstr[1]='\0';
-                        strcat(argstr,++*argv); /* glob argstr*/
-                        argc--;
-                        while((argc>0)&&(strlen(*argv)<3)&&
-				((*(argv+1))[0]==opsep)&&((*(argv+1))[1]!=opsep)){
-                                ++argv;
-                                argc--;
-                                strcat(argstr,++*argv);
-                        }
-                        ++argv; /* ok, consecutive flags now globbed into one*/
-                       c = find_opt(argstr+1); /* look up flag code */
-                switch(c){
+  if (--argc > 0)
+    {				/* Anything on command line ? */
+      ++argv;
+      while ((argc > 0) && ((*argv[0]) == opsep))
+	{			/* loop while 
+				   there are flags */
+	  argstr[0] = opsep;
+	  argstr[1] = '\0';
+	  strcat (argstr, ++*argv);	/* glob argstr */
+	  argc--;
+	  while ((argc > 0) && (strlen (*argv) < 3) &&
+		 ((*(argv + 1))[0] == opsep) && ((*(argv + 1))[1] != opsep))
+	    {
+	      ++argv;
+	      argc--;
+	      strcat (argstr, ++*argv);
+	    }
+	  ++argv;		/* ok, consecutive flags now globbed into one */
+	  c = find_opt (argstr + 1);	/* look up flag code */
+	  switch (c)
+	    {
 
 /* N.B. Your code should process command line arguments if
    they are expected. Make sure to increment argv and decrement argc 
@@ -59,25 +65,27 @@ if(--argc > 0){  /* Anything on command line ? */
    go in the case for that flag. 
 */
 
-			case -1: break;  /* default, e.g no options */
+	    case -1:
+	      break;		/* default, e.g no options */
 
-			case 0: printf("%s\n",info);
-				return 0;
+	    case 0:
+	      printf ("%s\n", info);
+	      return 0;
 /*  YOUR CASES HERE */
-                        case -2: ;  /* barf (don't use in your code) */
-			default:
-				fprintf(stderr,"%s: illegal option\n",argstr);
-				fprintf(stderr,USAGE);
-				return 1;
-			} /* End flag switch */
-	} /* End flag grabbing while loop */
-} /* end if: any args ? */
+	    case -2:;		/* barf (don't use in your code) */
+	    default:
+	      fprintf (stderr, "%s: illegal option\n", argstr);
+	      fprintf (stderr, USAGE);
+	      return 1;
+	    }			/* End flag switch */
+	}			/* End flag grabbing while loop */
+    }				/* end if: any args ? */
 
 /*   BODY OF PROGRAM GOES HERE  */
 /*   argc now gives a count of remaining arguments on command line,
 	and argv points at the first one */
 
-	return 0;
+  return 0;
 }
 
 /*  find_opt: return a unique small integer for each possible option string */
@@ -86,25 +94,27 @@ if(--argc > 0){  /* Anything on command line ? */
 
 /* N.B. word doesn't contain the leading hyphen */
 
-int find_opt(char *word)
+int
+find_opt (char *word)
 {
-	if(strcmp(word,"help")==0)return 0;
+  if (strcmp (word, "help") == 0)
+    return 0;
 
 /* arg not found */
-	return -2;
+  return -2;
 }
 
 /* myerror: generic error reporting routine */
 
-void myerror(char *fmt, ...)
+void
+myerror (char *fmt, ...)
 {
-        va_list args;
+  va_list args;
 
-        va_start(args,fmt);
-        fprintf(stderr,"%s: ",ProgramName);
-        vfprintf(stderr,fmt,args);
-        fprintf(stderr,"\n");
-        va_end(args);
-        exit(1);
+  va_start (args, fmt);
+  fprintf (stderr, "%s: ", ProgramName);
+  vfprintf (stderr, fmt, args);
+  fprintf (stderr, "\n");
+  va_end (args);
+  exit (1);
 }
-
---
+++ arg_template.md
@@ -0,0 +1,13 @@
+instructive to run indent and cppcheck on this source to show the compiler improvements and suggestions.
+
+try compilation without the <string.h>
+used on 
+gcc (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609
+
+and try 
+gcc -v
+
+after compilation try
+./arg_template  -as -q -z
+
+
